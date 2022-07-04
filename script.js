42681dc53636afeae7bd132acdaab3451bb676b4
Nayzawthant <108381970+Nayzawthant@users.noreply.github.com>
2022-07-04 06:26:03 -0700

    index.html

---
+++ script.js
@@ -0,0 +1,9 @@
+
+        const menuBtn = document.querySelector('.menu-btn')
+        const menuSecond = document.querySelector('.menuSecond')
+
+        menuBtn.addEventListener('click',()=>{
+            menuSecond.classList.toggle('mobile-menu')
+        });
+
+
